1fd220523b994a04ce8313015db291436835326e
Dan Branton <dtotheb@gmail.com>
2012-07-29 19:11:42 -0400

    initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,46 @@
+import sys
+import pygame
+from pygame.locals import *
+
+#setup pygame
+pygame.init()
+
+#setup the window
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('Hello World!')
+
+#setup the colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+
+#setup the fonts
+basicFont = pygame.font.SysFont(None, 48)
+
+#setup the text
+text = basicFont.render('Hello World!', True, WHITE, BLUE)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centerx
+textRect.centery = windowSurface.get_rect().centery
+
+windowSurface.fill(WHITE)
+
+pygame.draw.polygon(windowSurface, GREEN,
+    ((146, 0),
+     (291, 106),
+     (236, 277),
+     (56, 277),
+     (0, 106))
+    )
+
+windowSurface.blit(text, textRect)
+
+pygame.display.update()
+
+#run the game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
